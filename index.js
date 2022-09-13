4791fc39862201951db42505300af047508a6a64
jorge zubieta <zubietajor@gmail.com>
2022-09-13 05:58:49 -0300

    index.js agregado, estilos mejorados

---
+++ index.js
@@ -0,0 +1,15 @@
+// caso combiando
+let numero = 2;
+let numero2 = 5;
+
+// tma asignaciones combinadas
+
+let resultado = numero + nuemero2;
+console.log(resultado); 
+// sale e el consola
+
+// para que salga en el html usamos el  document.....
+
+// dato por consola
+// azul numerico
+// blanco String
